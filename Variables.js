d6316b51bb84f44187a311cca6cb21191cd3a8e2
Akshay kumar <akshaykakran2005@gmail.com>
2026-05-14 07:25:05 +0530

    Add comments for variable declarations in Variables.js
    Added comments explaining the use of var, let, and const.

--- Variables.js
+++ Variables.js
@@ -7,4 +7,6 @@ accCity="Jaipur";
 console.log(accId);
 
 
-
+//  var: Declares variables with function or global scope and allows re-declaration and updates within the same scope.
+//  let: Declares variables with block scope, allowing updates but not re-declaration within the same block.
+//  const: Declares block-scoped variables that cannot be reassigned after their initial assignment.
